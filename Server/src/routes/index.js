7160ca32239b35b4a20f8661632b05fda3d72ca3
Uristides <120599091+Uristides@users.noreply.github.com>
2024-07-06 23:11:37 -0600

    Base del repo

---
+++ Server/src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+//const accionDeTuta = require("./countryRouter");
+
+const router = Router();
+
+//router.use("/laRuta", accionDeRuta);
+
+module.exports = router;
