a9e732bdffaccd81a4bdf5658de734b07fc5ac0c
luismoralesunach <luismorales2914@gmail.com>
2024-07-09 16:28:16 -0600

    agregar card y cards y json

--- Client/src/Cards/Cards.jsx
+++ Client/src/Cards/Cards.jsx
@@ -1,19 +1,49 @@
+import { mangueras } from "../assets/data";
+import Card from "../Card/Card";
+import './Cards.css'
 
 
-const Card = ({name, brand, price, image})=>{
+
+const Cards = ()=>{
+
+    // "id": 1,
+    //   "image": "https://tauber.com.mx/storage/customer/images/312005_B7_STV_1_2_100.JPG",
+    //   "name": "Manguera1",
+    //   "price": 50.0,
+    //   "diameter": 1.5,
+    //   "longitude": {
+    //     "feet": [50.0, 100.0],
+    //     "meters": [15.24, 30.48]
+    // "brand": "MarcaA",
+    //   "stock": 10,
+    //   "type": "Doméstico",
+    //   "description": "Manguera de alta calidad para uso doméstico",
+    //   "available": true
+
+    const data = mangueras.filter(mang => mang.available === true && mang.stock > 0)
 
     return(
         <div>
-            <h1>{name}</h1>
-            <h2>{brand}</h2>
-            <h2>{price}</h2>
-            <img
-            src={image}
-            alt="manguera pic"
-            />
+
+            <div className="card-div">
+
+            {data.map(mang => (
+                <Card
+                id={mang.id}
+                name={mang.name}
+                price={mang.price}
+                diameter={mang.diameter}
+                length = {mang.longitude}
+                brand={mang.brand}
+                stock={mang.stock}
+                type={mang.type}
+                description={mang.description}
+                />
+                ))}
+                </div>
 
         </div>
     )
 }
 
-export default Card;
+export default Cards;
