0a377f43c784b7335d695aaab0ae67eedaf61c33
lucianofrailelopez <lucianofrailee@gmail.com>
2024-07-13 17:19:43 -0300

    LFL-1: Fix style Header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css'
-import { Login } from './views/login/login'
-import { Home } from './views/home/home'
-import Cart from './components/Cart/Cart'
-import Navbar from './components/Navbar/Navbar'
-import Detail from './components/Detail/Detail'
-import {Route, Routes} from 'react-router-dom'
-
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './views/login/login';
+import Navbar from './components/Navbar/Navbar';
+import { Home } from './views/home/home';
+import Cart from './components/Cart/Cart';
+import About from './components/About/about';
+import Detail from './components/Detail/Detail';
 
 function App() {
-  
   return (
     <>
       <Navbar />
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/cart' element={<Cart/>} />
-      <Route path='/detail/:id' element={<Detail/>} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/detail/:id' element={<Detail />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
