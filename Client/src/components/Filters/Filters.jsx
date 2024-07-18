e21d6689cdbc16fd5b3a3b3f5474fd402bc4febf
lucianofrailelopez <lucianofrailee@gmail.com>
2024-07-18 15:28:08 -0300

    Fixed filters

--- Client/src/components/Filters/Filters.jsx
+++ Client/src/components/Filters/Filters.jsx
@@ -1,18 +1,23 @@
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 import {
   fetchItems,
   filterItemsByPrice,
   filterByType,
-} from "../../redux/itemsSlice";
-import Sort from "../Sort/Sort";
-import styles from "./Filters.module.css";
+} from '../../redux/itemsSlice';
+import Sort from '../Sort/Sort';
+import styles from './Filters.module.css';
+import { useEffect, useState } from 'react';
 
 const Filters = () => {
   const dispatch = useDispatch();
+  const [type, setType] = useState(localStorage.getItem('filterType') || '');
+  const [price, setPrice] = useState(localStorage.getItem('filterPrice') || '');
 
   const handleFilterChange = (e) => {
     const { value } = e.target;
-    if (value === "") {
+    setType(value);
+    localStorage.setItem('filterType', value);
+    if (value === '') {
       dispatch(fetchItems());
     } else {
       dispatch(filterByType(value));
@@ -21,30 +26,44 @@ const Filters = () => {
 
   const handleFilterPrice = (e) => {
     const { value } = e.target;
-    if (value === "") {
+    setPrice(value);
+    localStorage.setItem('filterPrice', value);
+    if (value === '') {
       dispatch(fetchItems());
     } else {
       dispatch(filterItemsByPrice(value));
     }
   };
 
+  useEffect(() => {
+    if (type) {
+      dispatch(filterByType(type));
+    }
+
+    if (price) {
+      dispatch(filterItemsByPrice(price));
+    }
+  }, [dispatch, type, price]);
+
   return (
     <div className={styles.container}>
       <select
-        name="type"
+        name='type'
         onChange={handleFilterChange}
+        value={type}
         className={styles.select}
       >
-        <option value="">Tipos:</option>
-        <option value="Domestico">Domestico</option>
-        <option value="Jardineria">Jardineria</option>
-        <option value="Agricultura">Agricola</option>
+        <option value=''>Tipos:</option>
+        <option value='Domestico'>Domestico</option>
+        <option value='Jardineria'>Jardineria</option>
+        <option value='Agricultura'>Agricola</option>
       </select>
       <input
-        type="number"
-        name="price"
+        type='number'
+        name='price'
         onChange={handleFilterPrice}
-        placeholder="Rango de precio"
+        value={price}
+        placeholder='Rango de precio'
         className={styles.input}
       />
       <Sort />
--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,26 @@
 import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { searchItems, fetchItems } from '../../redux/itemsSlice';
 import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
   const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const savedSearchTerm = localStorage.getItem('searchTerm');
+    if (savedSearchTerm) {
+      setSearchTerm(savedSearchTerm);
+      dispatch(searchItems(savedSearchTerm));
+    } else {
+      dispatch(fetchItems());
+    }
+  }, [dispatch]);
+
   const handleSearchChange = (e) => {
     const { value } = e.target;
+    setSearchTerm(value);
+    localStorage.setItem('searchTerm', value);
     if (value === '') {
       dispatch(fetchItems());
     } else {
@@ -19,6 +34,7 @@ const SearchBar = () => {
       name='search'
       className={styles.search}
       placeholder='Nombre del producto'
+      value={searchTerm}
       onChange={handleSearchChange}
     />
   );
