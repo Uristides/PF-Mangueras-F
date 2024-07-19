10788312b44c7c3632e5654d2be4645acffe4acc
lucianofrailelopez <lucianofrailee@gmail.com>
2024-07-19 15:55:56 -0300

    Fix style of Detail component

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -1,15 +1,15 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import styles from './Detail.module.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './Detail.module.css';
 
 const Detail = () => {
   const { id } = useParams();
 
   const [product, setProduct] = useState(null);
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
-  console.log("Product in detail: ", product);
+  console.log('Product in detail: ', product);
 
   useEffect(() => {
     const getById = async (id) => {
@@ -17,7 +17,7 @@ const Detail = () => {
         const { data } = await axios.get(
           `http://localhost:3001/products/${id}`
         );
-        if (data) setProduct(data);        
+        if (data) setProduct(data);
       } catch (error) {
         console.log(error.message);
       }
@@ -30,56 +30,71 @@ const Detail = () => {
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Manguera de tipo: {product?.type}</h1>
 
       {product ? (
         <div className={styles.productContainer}>
+          <div className={styles.imageContainer}>
+            <img
+              src={product.image}
+              alt='product'
+              className={styles.productImage}
+            />
+          </div>
 
-            <div>
-            <img src={product.image} alt="product" className={styles.productImage}/>
-            </div>  
-
-            <div>
+          <div>
             <h1>{product.name}</h1>
             <p>{product.brand}</p>
-            <hr/>
+            <hr />
             <h3>$ {product.price}</h3>
-            <hr/>
+            <hr />
             <p>Marca: {product.brand}</p>
             <p>Diametro: {product.diameter}cm</p>
             <p>Tipo/Uso: {product.type}</p>
             <p>Acerca de: {product.description}</p>
-            </div>
+          </div>
 
-            <div className={styles.moneyContainer}>
-                <h2>{product.price} $</h2>
-                {product.available === true ? (
-                    <p style={{color: 'green'}}><strong>Disponible</strong></p>
-                    ): (<p style={{color: 'red'}}><strong>No Disponible</strong></p>)}
+          <div className={styles.moneyContainer}>
+            <h2>{product.price} $</h2>
+            {product.available === true ? (
+              <p style={{ color: 'green' }}>
+                <strong>Disponible</strong>
+              </p>
+            ) : (
+              <p style={{ color: 'red' }}>
+                <strong>No Disponible</strong>
+              </p>
+            )}
 
-                    {/* <option value="" disabled hidden>Cantidad: {quantity || 1}</option> */}
-                    <label>Cantidad: </label>
-
-                    <select 
-                    id="quantity-select"
-                    value={quantity}
-                    onChange={handleQuantityChange}
-                    >
-                    {product.available ? Array.from({ length: product.stock }, (_, index) => index + 1).map((num) => (
-                        <option key={num} value={num}>
-                            {num}
-                        </option>
-                        )) : <></>}
-                    </select>
-                    <br/>
-                <button 
-                className={styles.carritoButton}
-                disabled={!product.available}
-                >Agregar al Carrito</button>
-            </div>
-
-         
+            {/* <option value="" disabled hidden>Cantidad: {quantity || 1}</option> */}
+            <label>Cantidad: </label>
+            <select
+              id='quantity-select'
+              value={quantity}
+              onChange={handleQuantityChange}
+            >
+              {product.available ? (
+                Array.from(
+                  { length: product.stock },
+                  (_, index) => index + 1
+                ).map((num) => (
+                  <option key={num} value={num}>
+                    {num}
+                  </option>
+                ))
+              ) : (
+                <></>
+              )}
+            </select>
+            <br />
+            <button
+              className={styles.carritoButton}
+              disabled={!product.available}
+            >
+              Agregar al Carrito
+            </button>
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
