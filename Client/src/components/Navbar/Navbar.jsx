c95602774ad50c0048a1146e630ca609faf4d36e
lucianofrailelopez <lucianofrailee@gmail.com>
2024-07-21 19:38:42 -0300

    Fix styles and filters

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,78 +1,80 @@
-import { Link, useLocation } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar";
-import styles from "./Navbar.module.css";
-import { useContext } from "react";
-import { UserContext } from "../../App";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { Link, useLocation } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../App';
+import SearchBar from '../SearchBar/SearchBar';
+import styles from './Navbar.module.css';
 
 const Navbar = ({ sesion }) => {
   const { pathname } = useLocation();
-
   const { setUser, user } = useContext(UserContext);
 
   const deletCokie = (param) => {
     document.cookie =
-      param + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      param + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
   };
 
   const logout = async () => {
-    const out = await fetch("http://localhost:3001/user/logout", {
-      method: "POST",
-      credentials: "include",
-      header: { "Content-Type": "aplication/json" },
+    const out = await fetch('http://localhost:3001/user/logout', {
+      method: 'POST',
+      credentials: 'include',
+      header: { 'Content-Type': 'aplication/json' },
     });
     if (out.ok) {
       sesion();
-      deletCokie("lacookie");
+      deletCokie('lacookie');
       return location.reload();
     }
   };
 
   return (
     <div className={styles.main}>
-      
-      <Link to="/" className={styles.linksTitle}>
+      <Link to='/' className={styles.linksTitle}>
         The Hose Factory
       </Link>
-      <div className={styles.linksContainer}>
+      <div className={`${styles.linksContainer}`}>
         <Link
-          to="/"
-          className={`${styles.links} ${pathname === "/" ? styles.active : ""}`}
+          to='/'
+          className={`${styles.links} ${pathname === '/' ? styles.active : ''}`}
         >
           INICIO
         </Link>
-        <Link 
-          to="/cart"
+        <Link
+          to='/cart'
           className={`${styles.links} ${
-            pathname === "/cart" ? styles.active : ""
+            pathname === '/cart' ? styles.active : ''
           }`}
         >
           CARRO
         </Link>
         <Link
-          to="/about"
+          to='/about'
           className={`${styles.links} ${
-            pathname === "/about" ? styles.active : ""
+            pathname === '/about' ? styles.active : ''
           }`}
         >
           SOBRE NOSOTROS
         </Link>
         <Link
-          to="/admin"
+          to='/admin'
           className={`${styles.links} ${
-            pathname === "/admin" ? styles.active : ""
+            pathname === '/admin' ? styles.active : ''
           }`}
         >
           ADMINISTRADOR
         </Link>
+        {!user ? (
+          <Link to='/login' className={styles.loginButton}>
+            REGISTRARSE
+          </Link>
+        ) : (
+          <button className={styles.loginButton} onClick={logout}>
+            CERRAR SESIÓN
+          </button>
+        )}
       </div>
       <SearchBar />
-      {!user ? (
-        <Link to="/login" className={styles.loginButton}>
-          REGISTRARSE
-        </Link>
-      ) : (
-        <button className={styles.loginButton} onClick={logout}> CERRAR SESIÓN </button>
-      )}
     </div>
   );
 };
