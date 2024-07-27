9c1ac1155455481c83efc4076e5a7e0cf2b19be8
Uristides <120599091+Uristides@users.noreply.github.com>
2024-07-26 19:50:49 -0600

    arreglo de cantidades hacia el carrito

--- Client/src/components/AddRemoveCart/AddButton.jsx
+++ Client/src/components/AddRemoveCart/AddButton.jsx
@@ -1,22 +1,24 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../App';
-import { addToCart } from '../../redux/cartSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { MdOutlineShoppingCart, MdOutlineRemoveShoppingCart } from 'react-icons/md';
-import styles from './addButton.module.css';
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../App";
+import { addToCart, addOneToCart } from "../../redux/cartSlice"; // Import both actions
+import { useDispatch, useSelector } from "react-redux";
+import {
+  MdOutlineShoppingCart,
+  MdOutlineRemoveShoppingCart,
+} from "react-icons/md";
+import styles from "./addButton.module.css";
 
 const AddButton = (props) => {
   const stock = props.stock;
-  // console.log("Stock in addbutton props: ", stock);
 
   const dispatch = useDispatch();
-  const { user } = useContext(UserContext); 
+  const { user } = useContext(UserContext);
   const cartItems = useSelector((state) => state.cart.items); // Get cart items from the store
 
   // Initialize state with the user ID and an empty item field
   const [itemInfo, setItemInfo] = useState({
     id: user.id,
-    item: '',
+    item: "",
   });
 
   // Update the itemInfo state whenever props.data changes
@@ -27,29 +29,35 @@ const AddButton = (props) => {
     }));
   }, [props.data, user.id, cartItems]);
 
-  // Handle click event to dispatch the addToCart action
+  // Handle click event to dispatch the appropriate action based on the actionType prop
   const handleClick = () => {
-    const [productId, quantity] = props.data.split(':').map(Number);
+    const [productId, quantity] = props.data.split(":").map(Number);
     const existingItem = cartItems.find((i) => i.startsWith(`${productId}:`));
     let existingQuantity = 0;
 
     if (existingItem) {
-      existingQuantity = parseInt(existingItem.split(':')[1], 10);
+      existingQuantity = parseInt(existingItem.split(":")[1], 10);
       console.log("existingquantity in if(existingItem): ", existingQuantity);
-
     }
 
-    const newQuantity = existingQuantity + quantity;
+    const newQuantity =
+      existingQuantity + (props.actionType === "addOne" ? 1 : quantity);
+
     if (newQuantity <= stock) {
-      dispatch(addToCart({ userId: user.id, productId, quantity }))
+      const action =
+        props.actionType === "addOne"
+          ? addOneToCart({ userId: user.id, productId })
+          : addToCart({ userId: user.id, productId, quantity });
+
+      dispatch(action)
         .unwrap()
         .catch((error) => {
           console.error("Error in adding to cart: ", error.message);
           alert(error.message);
         });
     } else {
-      console.log('Cannot add more than available stock.');
-      alert('Cannot add more than available stock.');
+      console.log("Cannot add more than available stock.");
+      alert("Cannot add more than available stock.");
     }
   };
 
