cc5946345b15611010484a5692a8e1170e68e5b2
Uristides <120599091+Uristides@users.noreply.github.com>
2024-08-01 00:33:17 -0600

    mucho link

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,44 +1,41 @@
-import React, { useState, useEffect, createContext } from 'react';
-import { Login } from './views/login/login';
-import Home from './views/home/home';
-import About from './components/About/about';
-import Cart from './components/Cart/Cart';
-import Detail from './components/Detail/Detail';
-import Dashboard from './views/admin/Dashboard';
-import Navbar from './components/Navbar/Navbar';
-import Checkout from './components/Checkout/Checkout';
-import PaymentFeedback from './components/PaymentFeedback/PaymentFeedback';
-import Profile from './components/Profile/Profile';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { initMercadoPago } from '@mercadopago/sdk-react';
+import React, { useState, useEffect, createContext } from "react";
+import { Login } from "./views/login/login";
+import Home from "./views/home/home";
+import About from "./components/About/about";
+import Cart from "./components/Cart/Cart";
+import Detail from "./components/Detail/Detail";
+import Dashboard from "./views/admin/Dashboard";
+import Navbar from "./components/Navbar/Navbar";
+import Checkout from "./components/Checkout/Checkout";
+import PaymentFeedback from "./components/PaymentFeedback/PaymentFeedback";
+import Profile from "./components/Profile/Profile";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { initMercadoPago } from "@mercadopago/sdk-react";
+import OrderDetailsPageUser from "./components/UserOrder/UserOrder";
 
 const backendUrl = import.meta.env.VITE_BACKEND;
 
 export const UserContext = createContext(null);
 
 // Inicializa Mercado Pago con tu public key
-initMercadoPago('APP_USR-f9778cd5-2698-4783-954b-94f05d959a29', {
-  locale: 'es-MX',
+initMercadoPago("APP_USR-f9778cd5-2698-4783-954b-94f05d959a29", {
+  locale: "es-MX",
 });
 
 function App() {
-  const [user, setUser] = useState(() => {
-    // Recupera el usuario de localStorage al inicializar el estado
-    const savedUser = localStorage.getItem('user');
-    return savedUser ? JSON.parse(savedUser) : false;
-  });
-  const [searchTerm, setSearchTerm] = useState('');
+  const [user, setUser] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const sesion = async () => {
     try {
       const data = await fetch(`${backendUrl}/user/protected`, {
-        credentials: 'include',
+        credentials: "include",
       });
       if (data.ok) {
         const userData = await data.json();
         setUser(userData);
         // Guarda el usuario en localStorage
-        localStorage.setItem('user', JSON.stringify(userData));
+        localStorage.setItem("user", JSON.stringify(userData));
       } else {
         return;
       }
@@ -51,12 +48,12 @@ function App() {
     const handleBeforeUnload = (event) => sesion();
     const handleLoad = () => sesion();
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    window.addEventListener('load', handleLoad);
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    window.addEventListener("load", handleLoad);
 
     return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-      window.removeEventListener('load', handleLoad);
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      window.removeEventListener("load", handleLoad);
     };
   }, []);
 
@@ -66,30 +63,34 @@ function App() {
         <Navbar sesion={sesion} onSearch={setSearchTerm} />
         <Routes>
           <Route
-            path='/'
+            path="/"
             element={<Home sesion={sesion} searchTerm={setSearchTerm} />}
           />
           {user ? (
             <>
-              <Route path='/cart' element={<Cart />} />
-              <Route path='/checkout' element={<Checkout />} />
-              <Route path='/checkout/feedback' element={<PaymentFeedback />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/checkout/feedback" element={<PaymentFeedback />} />
               <Route
-                path='/profile'
+                path="/profile"
                 element={<Profile data={user} sesion={sesion} />}
+              />
+              <Route
+                path="/profile/order/:orderId"
+                element={<OrderDetailsPageUser data={user} sesion={sesion} />}
               />
             </>
           ) : (
-            <Route path='/cart' element={<Login sesion={sesion} />} />
+            <Route path="/cart" element={<Login sesion={sesion} />} />
           )}
-          {user && user.rol === 'Admin' ? (
-            <Route path='/admin/*' element={<Dashboard sesion={sesion} />} />
+          {user && user.rol === "Admin" ? (
+            <Route path="/admin/*" element={<Dashboard sesion={sesion} />} />
           ) : (
-            <Route path='/cart' element={<Navigate to='/login' />} />
+            <Route path="/cart" element={<Navigate to="/login" />} />
           )}
-          <Route path='/detail/:id' element={<Detail />} />
-          <Route path='/login' element={<Login sesion={sesion} />} />
-          <Route path='/about' element={<About />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/login" element={<Login sesion={sesion} />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </UserContext.Provider>
     </>
--- Client/src/components/Profile/Profile.jsx
+++ Client/src/components/Profile/Profile.jsx
@@ -1,23 +1,25 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState, useCallback, useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import {
   fetchUserById,
   editUser,
   fetchUserReviews,
   fetchUserOrders,
-} from '../../redux/userSlice';
-import EditModal from '../Profile/editModal/EditModal';
-import ConfirmModal from '../Profile/confirmModal/ConfirmModal';
-import { BsPersonCircle } from 'react-icons/bs';
-import { CiEdit } from 'react-icons/ci';
-import { AiOutlineUserDelete } from 'react-icons/ai';
-import styles from './Profile.module.css';
+} from "../../redux/userSlice";
+import EditModal from "../Profile/editModal/EditModal";
+import ConfirmModal from "../Profile/confirmModal/ConfirmModal";
+import { UserContext } from "../../App";
+import { BsPersonCircle } from "react-icons/bs";
+import { CiEdit } from "react-icons/ci";
+import { AiOutlineUserDelete } from "react-icons/ai";
+import styles from "./Profile.module.css";
 
 const Profile = ({ data, sesion }) => {
   const dispatch = useDispatch();
-  localStorage.setItem('user', JSON.stringify(data));
-  const userID = JSON.parse(localStorage.getItem('user'));
+  const { user } = useContext(UserContext);
+  localStorage.setItem("user", JSON.stringify(data));
+  const userID = user.id;
   const [showModalEdit, setShowModalEdit] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const dataUser = useSelector((state) => state.user.item);
@@ -26,9 +28,9 @@ const Profile = ({ data, sesion }) => {
   const orders = useSelector((state) => state.user.orders);
 
   useEffect(() => {
-    dispatch(fetchUserById(userID.id));
-    dispatch(fetchUserReviews(userID.id));
-    dispatch(fetchUserOrders(userID.id));
+    dispatch(fetchUserById(userID));
+    dispatch(fetchUserReviews(userID));
+    dispatch(fetchUserOrders(userID));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -39,11 +41,11 @@ const Profile = ({ data, sesion }) => {
 
   const formatDateTime = (isoString) => {
     const date = new Date(isoString);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = String(date.getFullYear()).slice(-2);
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
 
     return `${day}/${month}/${year} - ${hours}:${minutes}hs`;
   };
@@ -94,12 +96,17 @@ const Profile = ({ data, sesion }) => {
           <div className={styles.purchaseHistory}>
             {orders ? (
               orders.slice(-3).map((item, index) => (
-                <div className={styles.purchaseHistoryItem} key={index}>
-                  <p>N° de pedido: {item?.id.slice(0, 7)}...</p>
-                  <p>Monto: ${item?.amount}</p>
-                  <p>Fecha: {formatDateTime(item?.creation_date)}</p>
-                  <button className={styles.detailButton}>Ver detalle</button>
-                </div>
+                <Link
+                  to={`/profile/order/${item.id}`}
+                  key={index}
+                  className={styles.purchaseHistoryItemLink}
+                >
+                  <div className={styles.purchaseHistoryItem}>
+                    <p>N° de pedido: {item?.id.slice(0, 7)}...</p>
+                    <p>Monto: ${item?.amount}</p>
+                    <p>Fecha: {formatDateTime(item?.creation_date)}</p>
+                  </div>
+                </Link>
               ))
             ) : (
               <div className={styles.purchaseHistoryItem}>
@@ -111,20 +118,27 @@ const Profile = ({ data, sesion }) => {
           <div className={styles.purchaseHistory}>
             {dataReviews.length ? (
               dataReviews?.map((item, index) => (
-                <div className={styles.purchaseHistoryItem} key={index}>
-                  <p className={styles.comment}>
-                    {item.comment === '' ? 'No hay comentarios' : item.comment}
-                  </p>
-                  <p>Rating: {'⭐'.repeat(item.rating)}</p>
-                  <p>
-                    Producto:{' '}
-                    {
-                      products.filter(
-                        (product) => product.id === item.mangueraId
-                      )[0]?.name
-                    }
-                  </p>
-                </div>
+                <Link
+                  to={`/detail/${item.mangueraId}`}
+                  className={styles.purchaseHistoryItemLink}
+                >
+                  <div className={styles.purchaseHistoryItem} key={index}>
+                    <p>Rating: {"⭐".repeat(item.rating)}</p>
+                    <p>
+                      Producto:{" "}
+                      {
+                        products.find(
+                          (product) => product.id === item.mangueraId
+                        )?.name
+                      }
+                    </p>
+                    <p className={styles.comment}>
+                      {item.comment === ""
+                        ? "No hay comentarios"
+                        : item.comment}
+                    </p>
+                  </div>
+                </Link>
               ))
             ) : (
               <div className={styles.purchaseHistoryItem}>
