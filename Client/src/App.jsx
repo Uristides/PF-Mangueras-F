a808cf504a0c4baf0d433e78ce2bf8577040ea39
Uristides <120599091+Uristides@users.noreply.github.com>
2024-08-05 02:55:45 -0600

    arreglo feedBack

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -16,18 +16,18 @@ const backendUrl = import.meta.env.VITE_BACKEND;
 
 export const UserContext = createContext(null);
 
-
-initMercadoPago('APP_USR-f9778cd5-2698-4783-954b-94f05d959a29', {
-  locale: 'es-MX',
+initMercadoPago("APP_USR-f9778cd5-2698-4783-954b-94f05d959a29", {
+  locale: "es-MX",
 });
 
 function App() {
-  const [user, setUser] = useState(false
+  const [user, setUser] = useState(
+    false
     // Recupera el usuario de localStorage al inicializar el estado
     //const savedUser = localStorage.getItem('user');
     //return savedUser ? JSON.parse(savedUser) : false;
   );
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const sesion = async () => {
     try {
@@ -88,7 +88,6 @@ function App() {
           <Route path="/detail/:id" element={<Detail />} />
           <Route path="/login" element={<Login sesion={sesion} />} />
           <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </UserContext.Provider>
     </>
